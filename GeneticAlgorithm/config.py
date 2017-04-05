7144e56bb56c965856d9181093e8991642569626
nonprofitgibi <gibispace@gmail.com>
2017-04-05 18:31:29 -0400

    Update config.py
    Corrected crash recovery

--- GeneticAlgorithm/config.py
+++ GeneticAlgorithm/config.py
@@ -5,23 +5,22 @@ import reporter
 import re
 
 def run(fileName:str):
-    local_dir = os.path.dirname(__file__)
     checkpoint = None
     checkpointNum = 0
-    for file in os.listdir(local_dir):
+    for file in os.listdir('.'):
         results = re.search("neat-checkpoint-(\d+)", file)
         if results and results.group(1).isdigit():
             if checkpointNum < int(results.group(1)):
                 checkpointNum = int(results.group(1))
                 checkpoint = results.group(0)
     if checkpoint is not None:
-        config_path = os.path.join(local_dir, checkpoint)
+        config_path = os.path.join('.', checkpoint)
         population = neat.Checkpointer.restore_checkpoint(config_path)
     else:
         config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
                              neat.DefaultStagnation, fileName)
         population = neat.Population(config)
-
+    
     # Adding reporters so we have some output
     population.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
